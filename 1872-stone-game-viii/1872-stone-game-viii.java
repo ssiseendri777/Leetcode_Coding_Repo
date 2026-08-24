a3e7153a3075a14251d32940b85cf775db5e9cef
S SISEENDRI <ssiseendri777@gmail.com>
2026-08-24 17:19:25 +0530

    1872-stone-game-viii 08-24-2026 .java Hard 4 ms (66.81%) 83.4 MB (84.26%)

---
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public static int stoneGameVIII(int[] stones) {
+        // prefix sum conversion of stones
+        for(int i = 1; i < stones.length;i++){
+            stones[i] += stones[i-1];
+        }
+        // condition alice can take all stones
+        int dp = stones[stones.length -1];
+        // check better prefix from right to left 
+        for(int i = stones.length-2;i>0;i--){
+            // current gain = dp 
+            // opponents best advantage = stones[i] - dp
+            dp = Math.max(dp,stones[i]-dp);
+            // keeping the better option
+        }
+        return dp;
+    }
+    public static void main(String[] args){
+        int[] arr = {-1,-1,-3,4,-10,-6,19,6,8};
+        System.out.print("Output : "+stoneGameVIII(arr));
+    }
+}
