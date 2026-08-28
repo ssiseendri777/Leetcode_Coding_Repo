e951e2ce37759c1996d4734162f1f4c18ca0402a
S SISEENDRI <ssiseendri777@gmail.com>
2026-08-28 19:59:14 +0530

    0258-add-digits 08-28-2026 .java Easy 1 ms (98.05%) 42.7 MB (44.92%)

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,18 +1,12 @@
 class Solution {
-    public static int addDigits(int num){
-        int result = 0;
-        while(num != 0){
-            result += (num % 10);
-            num /= 10;
-            if(num == 0 && result > 9){
-                num = result;
-                result = 0;                
-            }
-        }
-        return result;
+    public static int addDigits(int num) {
+        if (num == 0)
+            return 0;
+        return 1 + (num - 1) % 9; 
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         int num = 54673;
-        System.out.println("The Digit sum of "+num+" is : "+addDigits(num));
+        System.out.println("The Digit sum of " + num + " is : " + addDigits(num));
     }
 }
