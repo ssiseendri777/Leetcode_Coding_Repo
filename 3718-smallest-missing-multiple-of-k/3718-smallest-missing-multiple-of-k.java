b167ab9aed5f65014e71958b25d6b2f959d4f398
S SISEENDRI <ssiseendri777@gmail.com>
2026-09-01 11:50:27 +0530

    3718-smallest-missing-multiple-of-k 09-01-2026 .java Easy 0 ms (100%) 45 MB (78.54%)

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -1,22 +1,23 @@
-import java.util.HashSet;
-import java.util.*;
-
 class Solution {
     public static int missingMultiple(int[] nums, int k) {
-        HashSet<Integer> arr = new HashSet<>();
-        for(int num : nums){
-            if(num > 0 && num % k == 0){
-                arr.add(num);
+        int n = nums.length;
+        for (int i = k; i <= k * (n + 1); i += k) {
+            boolean flag = false;
+            for (int num : nums) {
+                if (num == i) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) {
+                return i;
             }
         }
-        int candidate = k;
-        while(arr.contains(candidate)){
-            candidate += k;
-        }
-        return candidate;
+        return k * (n + 1);
     }
-    public static void main(String[] args){
-        int[] arr = {2,3,4,8,10,12};
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 4, 8, 10, 12 };
         int k = 2;
         System.out.println("The smallest positive multiple of " + k + " is : " + missingMultiple(arr, k));
     }
