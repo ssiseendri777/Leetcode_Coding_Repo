7dcb6c1db92010339eaefdb12857aa42920bc99d
S SISEENDRI <ssiseendri777@gmail.com>
2026-09-05 11:04:11 +0530

    3903-smallest-stable-index-i 09-05-2026 .java Easy 1 ms (99.79%) 45.8 MB (99.37%)

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -4,18 +4,6 @@ class Solution {
         // created the array to store the stable index for each index i of nums
         int[] stableindex = new int[n];
         // used to compare
-        int max = Integer.MIN_VALUE;
-        //stable index formula = max(nums[0..i]) - min(nums[i..n - 1])
-        // for each i it stores the max(nums[0...i]) of the formula
-        /* started from first index because
-            for max term if we see from left to right 0 to 0...n-1 the number of index increases
-            for i = 0 stable index = max(nums[0]) - min(nums[0..n - 1] ..... so on up to 
-            for i = n-1 stable index = max(nums[0...n-1]) - min(nums[n - 1]
-        */
-        for (int i = 0; i < n; i++) {
-            max = Math.max(max, nums[i]);
-            stableindex[i] = max;
-        }
         // used to compare
         int min = Integer.MAX_VALUE;
         // for each i it substract min(nums[i...n-1]) from the max(nums[0...i]) as per formula
@@ -26,10 +14,19 @@ class Solution {
         */
         for (int i = n - 1; i >= 0; i--) {
             min = Math.min(min, nums[i]);
-            stableindex[i] -= min;
+            stableindex[i] = min;
         }
+        int max = Integer.MIN_VALUE;
+        //stable index formula = max(nums[0..i]) - min(nums[i..n - 1])
+        // for each i it stores the max(nums[0...i]) of the formula
+        /* started from first index because
+            for max term if we see from left to right 0 to 0...n-1 the number of index increases
+            for i = 0 stable index = max(nums[0]) - min(nums[0..n - 1] ..... so on up to 
+            for i = n-1 stable index = max(nums[0...n-1]) - min(nums[n - 1]
+        */
         for (int i = 0; i < n; i++) {
-            if (stableindex[i] <= k) {
+            max = Math.max(max, nums[i]);
+            if ((max - stableindex[i]) <= k) {
                 return i;
             }
         }
