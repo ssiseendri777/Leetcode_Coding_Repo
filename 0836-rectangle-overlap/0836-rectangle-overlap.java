c12a79b4df38e90d5fc996afc03c3c85fb5e212d
S SISEENDRI <ssiseendri777@gmail.com>
2026-09-14 10:25:08 +0530

    0836-rectangle-overlap 09-14-2026 .java Easy 0 ms (100%) 42.1 MB (98.23%)

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        // condition for not a rectangle or checking it is a line 
+        if (rec1[0] == rec1[2] || rec1[1] == rec1[3] || rec2[0] == rec2[2] || rec2[1] == rec2[3]) {
+            return false;
+        }
+
+        return !(rec1[2] <= rec2[0] || // left
+                rec1[3] <= rec2[1] || // bottom
+                rec1[0] >= rec2[2] || // right
+                rec1[1] >= rec2[3]); // top
+
+    }
+
+    public static void main(String[] args) {
+        int[] rect1 = { 0, 1, 2, 3 }, rect2 = { 1, 0, 3, 2 };
+        System.out.println("Is rectangle 1 and 2 overlap : " + isRectangleOverlap(rect1, rect2));
+
+    }
+}
